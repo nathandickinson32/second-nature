1d955b2fc20d668885f51898b8fbc8953d63a4b4
ziechn <zedziechn@gmail.com>
2024-07-15 13:09:35 -0400

    Added notes and images. Created the second_nature database. Run sh create.sh.

--- Second-Nature/java/database/data.sql
+++ Second-Nature/java/database/data.sql
@@ -1,6 +1,12 @@
 BEGIN TRANSACTION;
 
-INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
-INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
+INSERT INTO users (username,password_hash,role,first_name,last_name,title)
+VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER', 'User First Name', 'User Last Name', 'Default Title');
+
+INSERT INTO users (username,password_hash,role,first_name,last_name,title)
+VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN', 'Admin First Name', 'Admin Last Name', 'Default Title');
+
+INSERT INTO time_off_requests (user_id, request_date, start_date, end_date, status, request_reason, comment)
+VALUES (1, '2024-07-15', '2024-07-22', '2024-07-26', 'approved', 'Need time off.', 'Take all the time you need.');
 
 COMMIT TRANSACTION;
--- Second-Nature/java/database/schema.sql
+++ Second-Nature/java/database/schema.sql
@@ -1,5 +1,6 @@
 BEGIN TRANSACTION;
 
+DROP TABLE IF EXISTS time_off_requests;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
@@ -7,7 +8,22 @@ CREATE TABLE users (
 	username varchar(50) NOT NULL UNIQUE,
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) NOT NULL,
+	first_name VARCHAR (100) NOT NULL,
+	last_name VARCHAR (100) NOT NULL,
+	title VARCHAR (100) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
+);
+
+CREATE TABLE time_off_requests (
+    request_id SERIAL PRIMARY KEY,
+    user_id INT,
+    request_date DATE NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    status VARCHAR (8),
+    request_reason VARCHAR (200),
+    comment VARCHAR (200),
+    FOREIGN KEY (user_id) REFERENCES users (user_id)
 );
 
 COMMIT TRANSACTION;
