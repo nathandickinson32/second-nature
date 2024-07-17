9ce93c55c66d8c772b5b8820d57c72b297481808
Kristi <kristi.n.norris777@gmail.com>
2024-07-16 19:00:34 -0700

    Made manager title for Boss (Boss1), added boss to create.sh script with title manager, updated store to check for manager; conditional elements now possible for managers

--- Second-Nature/vue/src/views/HomeView.vue
+++ Second-Nature/vue/src/views/HomeView.vue
@@ -3,6 +3,11 @@
     <h1>Second Nature</h1>
     <p>Welcome, {{ this.$store.state.user.firstName }}!</p>
     <p>App Squares go here</p>
+    <!-- <div >Instead of having two landing pages for employees vs managers, 
+      What if we had the buttons for the manager pages tagged with v-show
+    </div> -->
+    <div v-show="isManager">Review requests off</div>
+    
     <div id="menu-grid">
       <span>testing</span>
       <span>testing</span>
@@ -18,6 +23,11 @@
 
 <script>
 export default {
+  computed: {
+    isManager() {
+      return this.$store.getters.isManager;
+    }
+  }
 };
 </script>
 
