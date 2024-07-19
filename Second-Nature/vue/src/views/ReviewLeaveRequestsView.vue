da1fe87a17ea8cdf47adc8d9179373046262b08d
Kristi <kristi.n.norris777@gmail.com>
2024-07-19 14:29:07 -0700

    Stopped using the component for reviewing leave requests- got better answer

--- Second-Nature/vue/src/views/ReviewLeaveRequestsView.vue
+++ Second-Nature/vue/src/views/ReviewLeaveRequestsView.vue
@@ -1,7 +1,18 @@
 <template>
     <!-- <LeaveRequestList :request="requests"  /> -->
     <div v-for="request in requests" v-bind:key="request.id" class="request-card">
-        <LeaveRequestList :request="request"  />
+        <!-- <LeaveRequestList :request="request"  /> -->
+         <div class="container">
+            <div class="requestCard">
+                <p>Date Requested: {{ request.requestDate }}</p>
+                <p>Start Date: {{ request.startDate }}</p>
+                <p>End Date: {{ request.endDate }}</p>
+                <p>Reason: {{ request.requestReason }}</p>
+                <p>Status: {{ request.status }}</p>
+                <p v-if="request.status != 'Pending'">Manager comment: {{ request.comment }}</p>
+                <p>Reviewed by manager: {{ request.reviewDate }}</p>
+            </div>
+        </div>
         <div class="action-buttons">
             <button class="approve-button" @click="approveRequest(request)">Approve</button>
             <button class="deny-button" @click="denyRequest(request)">Deny</button>
@@ -12,11 +23,11 @@
 <script>
 
 import LeaveRequestService from '../services/LeaveRequestService';
-import LeaveRequestList from '../components/LeaveRequestList.vue';
+// import LeaveRequestList from '../components/LeaveRequestList.vue';
 
 export default {
     components: {
-        LeaveRequestList
+        // LeaveRequestList
     },
     data() {
         return {
@@ -52,6 +63,7 @@ export default {
   align-items: center;
   margin-bottom: 10px;
 }
+
 
 .action-buttons {
   display: flex;
