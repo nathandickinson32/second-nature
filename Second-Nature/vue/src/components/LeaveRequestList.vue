79771396a869c3c0daae9a7317bcc804b95f2225
obrienda1 <dinda.obrien@gmail.com>
2024-07-19 14:06:35 -0400

    started Leave Request List component

---
+++ Second-Nature/vue/src/components/LeaveRequestList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Leave Requests</h1>
+    <div v-for="request in requests" v-bind:key="request.id">
+        
+    </div>
+  </div>
+</template>
+
+<script>
+import LeaveRequestService from '../services/LeaveRequestService';
+
+export default {
+    data() {
+        return {
+            requests: []
+        }
+    },
+    created() {
+        LeaveRequestService.getAllTimeOffRequestsByUserId()
+        .then((response) => {
+            console.log(response);
+        })
+    }
+}
+
+</script>
+
+<style>
+
+</style>
