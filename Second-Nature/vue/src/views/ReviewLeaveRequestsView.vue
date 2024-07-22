1c877a88126f4431439f27d94f09948a3c4e3950
obrienda1 <dinda.obrien@gmail.com>
2024-07-22 10:02:26 -0400

    formatting review leave requests page

--- Second-Nature/vue/src/views/ReviewLeaveRequestsView.vue
+++ Second-Nature/vue/src/views/ReviewLeaveRequestsView.vue
@@ -11,12 +11,13 @@
                 <p>Status: {{ request.status }}</p>
                 <p v-if="request.status != 'Pending'">Manager comment: {{ request.comment }}</p>
                 <p>Reviewed by manager: {{ request.reviewDate }}</p>
-            </div>
+                <span class="action-buttons">
+                    <button class="approve-button" @click="approveRequest(request)">Approve</button>
+                    <button class="deny-button" @click="denyRequest(request)">Deny</button>
+                </span>
+            </div>    
         </div>
-        <div class="action-buttons">
-            <button class="approve-button" @click="approveRequest(request)">Approve</button>
-            <button class="deny-button" @click="denyRequest(request)">Deny</button>
-        </div>
+        
     </div>
 </template>
 
@@ -56,27 +57,49 @@ export default {
 
 </script>
 
-<style>
-.request-card {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 10px;
+<style scoped>
+
+.container {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: space-around;
+    gap: 30px;
+}
+.requestCard {
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    border: 1px solid black;
+    border-radius: 5px;
+    padding: 10px;
+    padding-left: 20px;
+    margin-top:10px;
+    margin-bottom: 10px;
+    width: 300px;
 }
 
+p {
+    margin: 2px;
+    padding: 2px;
+}
 
 .action-buttons {
   display: flex;
   gap: 10px;
+  padding-top: 3px;
+  align-self: center;
 }
 
 .deny-button {
     background-color: red;
     color: white;
+    border-radius: 5px;
 }
 
 .approve-button {
     background-color: green;
     color: white;
+    border-radius: 5px;
 }
 </style>
