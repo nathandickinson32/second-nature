36bff4963b2a1a6946acd744c566a4608b1cd749
ziechn <zedziechn@gmail.com>
2024-07-24 09:53:19 -0400

    Added an authorized endpoint to get user by ID.

--- Second-Nature/java/src/main/java/com/techelevator/controller/UserController.java
+++ Second-Nature/java/src/main/java/com/techelevator/controller/UserController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
+import java.time.LocalDateTime;
 
 @RestController
 @CrossOrigin
@@ -20,6 +22,13 @@ public class UserController {
 
     @GetMapping(path = "/get-user")
     public User getUser(Principal principal){
+        System.out.println(LocalDateTime.now() + " [User Controller] Accessing user " + principal.getName());
         return userDao.getUserByUsername(principal.getName());
+    }
+
+    @GetMapping(path = "/get-user/{id}")
+    public User getUserById(@PathVariable int id, Principal principal){
+        System.out.println(LocalDateTime.now() + " [User Controller] Accessing user id: " + id + " by user: " + principal.getName());
+        return userDao.getUserById(id);
     }
 }
