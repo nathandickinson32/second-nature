6969a1daeb6ecee7d39b572e856873c5926d6238
ziechn <zedziechn@gmail.com>
2024-07-26 12:07:34 -0400

    Created a Kudos model.

---
+++ Second-Nature/java/src/main/java/com/techelevator/model/Kudos.java
@@ -0,0 +1,49 @@
+package com.techelevator.model;
+
+public class Kudos {
+    private int kudosId;
+    private int giverUserId;
+    private int receiverUserId;
+    private String title;
+    private String notes;
+
+    public int getKudosId() {
+        return kudosId;
+    }
+
+    public void setKudosId(int kudosId) {
+        this.kudosId = kudosId;
+    }
+
+    public int getGiverUserId() {
+        return giverUserId;
+    }
+
+    public void setGiverUserId(int giverUserId) {
+        this.giverUserId = giverUserId;
+    }
+
+    public int getReceiverUserId() {
+        return receiverUserId;
+    }
+
+    public void setReceiverUserId(int receiverUserId) {
+        this.receiverUserId = receiverUserId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+}
