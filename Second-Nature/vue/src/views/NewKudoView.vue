25e8c5b08256e0f95fbb754292678680ebea88c1
ziechn <zedziechn@gmail.com>
2024-08-01 14:14:48 -0400

    Give kudos styling.

--- Second-Nature/vue/src/views/NewKudoView.vue
+++ Second-Nature/vue/src/views/NewKudoView.vue
@@ -1,12 +1,12 @@
 <template>
-    <h1>Give Kudos</h1>
     <div class="container">
+        <h2>Give Kudos</h2>
         <form id="give-kudos-form" @submit.prevent="onSubmit">
             <!-- Dropdown list of users -->
-            <div class="form-group">
-                <label for="userSelect">Select User:</label>
+            <div class="form-input-group">
+                <label for="userSelect">To: </label>
                 <select v-model="kudo.receiverUserId" id="userSelect" required>
-                    <option value="" disabled>Select a user</option>
+                    <option value="" disabled>Select a Team Member</option>
                     <option v-for="user in users" :key="user.userId" :value="user.userId">
                         {{ user.firstName }} {{ user.lastName }}
                     </option>
@@ -14,14 +14,13 @@
             </div>
 
             <!-- Title text field -->
-            <div class="form-group">
-                <label for="title">Title:</label>
+            <div class="form-input-group">
+                <label for="title">Title: </label>
                 <input type="text" v-model="kudo.title" id="title" required />
             </div>
 
             <!-- Note text field -->
-            <div class="form-group">
-                <label for="note">Note:</label>
+            <div class="form-input-group">
                 <textarea v-model="kudo.notes" id="note" rows="4" required></textarea>
             </div>
 
@@ -87,4 +86,38 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+button {
+    height: 50px;
+}
+
+form {
+    display: flex;
+    flex-direction: column;
+    background-color: white;
+    border: 1px solid #a1af9f;
+    border-radius: 10px;
+    padding: 20px;
+    width: 90%;
+    align-content: space-between;
+    justify-content: space-between;
+}
+
+h2 {
+    text-align: center;
+}
+
+#note {
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    width: 100%;
+    height: 20em;
+    resize: none;
+}
+
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 20px 5%;
+}
+</style>
