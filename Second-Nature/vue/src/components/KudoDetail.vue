c6e2b23faf3f7e9e96ab7ea5fe8f390e2f9bc99d
ziechn <zedziechn@gmail.com>
2024-08-01 13:49:12 -0400

    Kudos detail view styling.

--- Second-Nature/vue/src/components/KudoDetail.vue
+++ Second-Nature/vue/src/components/KudoDetail.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="kudo">
-    <h1>{{ kudo.title }}</h1>
-    <p>Given by: {{ giverUserName }}</p>
-    <p>Sent to: {{ receiverUserName }}</p>
-    <p>Sent on: {{ kudo.date }}</p>
-    <p>Note:</p>
-    <p class="kudo-note">{{ kudo.notes }}</p>
+  <div class="container">
+    <div class="kudo">
+      <h3>{{ kudo.title }}</h3>
+      <p class="kudo-note">{{ kudo.notes }}</p>
+      <span class="small-font">From: {{ giverUserName }}</span>
+      <span class="small-font">To: {{ receiverUserName }}</span>
+      <span class="small-font">Date: {{ kudo.date }}</span>
+      
+    </div>
   </div>
 </template>
 
@@ -31,17 +33,21 @@ export default {
 </script>
 
 <style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  margin: 20px 5%;
+  align-items: center;
+}
+
 .kudo {
   display: flex;
   flex-direction: column;
   justify-content: start;
   border: 1px solid #a1af9f;
   border-radius: 5px;
-  padding: 10px;
-  padding-left: 20px;
-  margin-top: 10px;
-  margin-bottom: 10px;
-  width: 300px;
+  padding: 20px;
+  width: 90%;
   background-color: white;
   box-shadow: -2px 2px 4px #a1af9f;
 }
