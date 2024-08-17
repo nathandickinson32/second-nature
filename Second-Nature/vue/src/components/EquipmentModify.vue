473d6df28db545493c36083b3f3af895ff45e007
Kristi <kristi.n.norris777@gmail.com>
2024-08-17 09:04:27 -0700

    Equipment modify now works!

--- Second-Nature/vue/src/components/EquipmentModify.vue
+++ Second-Nature/vue/src/components/EquipmentModify.vue
@@ -1,45 +1,36 @@
 <template>
-  <div class="large-container">
-    <form id="equipment-form" @submit.prevent="onSubmit">
-        <div class="form-input-group">
-            <label for="title">Serial Number: </label>
-            <input type="text" v-model="equipment.serialNumber" id="serialNumber" required />
-        </div>
-        <div class="form-input-group">
-            <label for="title">Model: </label>
-            <input type="text" v-model="equipment.model" id="model" required />
-        </div>
-        <div class="form-input-group">
-            <label for="title">Name: </label>
-            <input type="text" v-model="equipment.name" id="name" required />
+  <div class="content">
+    <div class="large-container">
+        <form id="equipment-form" @submit.prevent="onSubmit">
+            <div class="form-input-group">
+                <label for="title">Serial Number: </label>
+                <input type="text" v-model="updateEquipment.serialNumber" id="serialNumber" required />
+            </div>
+            <div class="form-input-group">
+                <label for="title">Model: </label>
+                <input type="text" v-model="updateEquipment.model" id="model" required />
+            </div>
+            <div class="form-input-group">
+                <label for="title">Name: </label>
+                <input type="text" v-model="updateEquipment.name" id="name" required />
 
-        </div>
-        <div class="form-input-group">
-            <label for="title">Current Hours: </label>
-            <input type="text" v-model="equipment.startingHours" id="startingHours" required />
+            </div>
+            <!-- <div class="form-input-group">
+                <label for="title">Current Hours: </label>
+                <input type="text" v-model="equipment.startingHours" id="startingHours" required />
+            </div> -->
+            <div class="form-input-group">
+                <label for="title">Notes: </label>
+                <input type="text" v-model="updateEquipment.notes" id="notes" />
 
-        </div>
-        <!-- <div class="form-input-group">
-            <label for="title">Currently Active: </label>
-            <input type="text" v-model="equipment.active" id="active" required />
-
-        </div>
-        <div class="form-input-group">
-            <label for="title">Notes about Active Status: </label>
-            <input type="text" v-model="equipment.activeNotes" id="activeNotes" required />
-
-        </div> -->
-        <div class="form-input-group">
-            <label for="title">Notes: </label>
-            <input type="text" v-model="equipment.notes" id="notes" />
-
-        </div>
-        <div class="button-section">
-                <label @click="goBack" class="clickable-label">Back to Equipment Details</label>
-                <span class="separator"> | </span>
-                <label @click="saveEquipment" class="clickable-label">Save Equipment Changes</label>
-        </div>
-    </form>
+            </div>
+        </form>
+    </div>
+    <div class="button-section">
+        <label @click="goBack" class="clickable-label">Back to Equipment Details</label>
+        <span class="separator"> | </span>
+        <label @click="saveEquipment" class="clickable-label">Save Equipment Changes</label>
+    </div>
   </div>
 </template>
 
@@ -72,16 +63,12 @@ export default {
                 serialNumber: '',
                 model: '',
                 name: '',
+                // hours: 0,
                 notes: '',
                 active: true,
                 activeNotes: '',
                 updatedByUserId: '',
                 archived: false
-            }, 
-            archivedEquipment: {
-                equipmentId: 0,
-                updatedByUserId: '',
-                archived: true
             }
         }
     },
@@ -89,6 +76,15 @@ export default {
         EquipmentService.getEquipmentById(this.$route.params.equipmentId).then((response) => {
             if (response.status == 200) {
                 this.equipment = response.data;
+                this.updateEquipment.equipmentId = this.equipment.equipmentId;
+                this.updateEquipment.serialNumber = this.equipment.serialNumber;    
+                this.updateEquipment.model = this.equipment.model;  
+                this.updateEquipment.name = this.equipment.name;
+                // this.updateEquipment.hours = this.equipment.startingHours;
+                this.updateEquipment.notes = this.equipment.notes;
+                this.updateEquipment.active = this.equipment.active;
+                this.updateEquipment.activeNotes = this.equipment.activeNotes;  
+                this.updateEquipment.archived = this.equipment.archived;
             }
         }).catch((error) => {
             console.log(error);
@@ -99,7 +95,7 @@ export default {
             this.$store.commit("SET_EQUIPMENT_DETAIL_VIEW", 'detail');
         },
         saveEquipment() {
-            EquipmentService.updateEquipment(this.equipmentActivity)
+            EquipmentService.updateEquipment(this.updateEquipment)
             .then(response => {
                 console.log(response.data);
                 alert('Equipment Modifications updated!');
