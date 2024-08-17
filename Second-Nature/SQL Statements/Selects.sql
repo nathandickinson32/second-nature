7c5bb73c93cd1d3c5c025dac0aa1130394bc4c16
nathandickinson32 <nathandickinson32@gmail.com>
2024-08-17 11:10:09 -0400

    sql statements

--- Second-Nature/SQL Statements/Selects.sql
+++ Second-Nature/SQL Statements/Selects.sql
@@ -1,5 +1,7 @@
 SELECT * FROM time_off_requests JOIN users ON time_off_requests.user_id = users.user_id WHERE users.username = 'user'
 SELECT * FROM users
 SELECT * FROM professional_check_in
-SELECT * FROM maintenance_ticket
+SELECT * FROM maintenance_tickets
 SELECT * FROM maintenance_performed
+SELECT * FROM equipment
+
