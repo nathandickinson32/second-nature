f1ef243119e2f25a73905c8c854d621cdb485c3e
obrienda1 <dinda.obrien@gmail.com>
2024-08-16 20:36:49 -0400

    started on MaintenanceDTO

--- Second-Nature/java/src/main/java/com/techelevator/controller/MaintenanceController.java
+++ Second-Nature/java/src/main/java/com/techelevator/controller/MaintenanceController.java
@@ -6,10 +6,7 @@ import com.techelevator.model.MaintenanceTicket;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 import java.time.LocalDateTime;
@@ -36,6 +33,9 @@ public class MaintenanceController {
     }
 
     // Update
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    @PostMapping(path = "/complete")
+    public
 
     // Delete
 }
