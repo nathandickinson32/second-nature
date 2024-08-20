f324b8b80a65ee02f43a0d8865062749cf176fcc
ziechn <zedziechn@gmail.com>
2024-08-20 16:22:43 -0400

    Updated button styling to add a new professional checkin.

--- Second-Nature/vue/src/views/MyProfessionalCheckInView.vue
+++ Second-Nature/vue/src/views/MyProfessionalCheckInView.vue
@@ -38,6 +38,20 @@ export default {
 </script>
 
 <style scoped>
+#add-check-in-btn {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid #2a64bd ;
+  border-radius: 10px;
+  background-color: white;
+  width: 100%;
+  max-width: 400px;
+  height: 50px;
+  text-align: center;
+  color: #2a64bd;
+}
+
 .content {
   flex-direction: column;
   align-items: center;
