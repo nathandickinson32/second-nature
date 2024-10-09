052c39aef3045883da590c6f3e28b53153e6b8be
nathandickinson32 <nathandickinson32@gmail.com>
2024-10-08 21:21:29 -0400

    created training resouceDTO

--- Second-Nature/java/src/main/java/com/techelevator/model/CreateTrainingResourceDTO.java
+++ Second-Nature/java/src/main/java/com/techelevator/model/CreateTrainingResourceDTO.java
@@ -1,4 +1,31 @@
 package com.techelevator.model;
 
 public class CreateTrainingResourceDTO {
+    private String category;
+    private String content;
+    private String resourceSource;
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getResourceSource() {
+        return resourceSource;
+    }
+
+    public void setResourceSource(String resourceSource) {
+        this.resourceSource = resourceSource;
+    }
 }
