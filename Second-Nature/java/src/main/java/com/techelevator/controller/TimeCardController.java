683fa94116d07e3b199f4abb8933670b9da2cc72
nathandickinson32 <nathandickinson32@gmail.com>
2024-11-02 14:38:21 -0400

    removed comments

--- Second-Nature/java/src/main/java/com/techelevator/controller/TimeCardController.java
+++ Second-Nature/java/src/main/java/com/techelevator/controller/TimeCardController.java
@@ -36,7 +36,6 @@ public class TimeCardController {
     @GetMapping(path = "/{id}")
     public TimeCards getTimeCardById(@PathVariable int id, Principal principal){
         System.out.println(LocalDateTime.now() + " User: " + principal.getName() + " accessed Time Card ID: " + id);
-        System.out.println(timeCardsDao.getTimeCardById(id).getDateTime());
         return timeCardsDao.getTimeCardById(id);
     }
     @ResponseStatus(HttpStatus.CREATED)
