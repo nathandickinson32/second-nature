2cdbe8375481f2108f9d2700139a482cfa9a7fbc
nathandickinson32 <nathandickinson32@gmail.com>
2024-11-19 18:54:56 -0500

    time stamp work. made method to compare dates to find out if today or yesterday. need to filter to justshow today and yesterday stamps

--- Second-Nature/vue/src/components/TIME-STAMP/TimeStamp.vue
+++ Second-Nature/vue/src/components/TIME-STAMP/TimeStamp.vue
@@ -1,59 +1,62 @@
 <template>
-  <h3>{{formatTime(timeCard.dateTimeIn )}} </h3>
-  <h3>Clocked In</h3>
-  <h3>{{formatTime(timeCard.dateTimeOut )}}</h3>
-  <h3>Clocked Out</h3>
-
+  <h3>{{ formatTime(timeCard.dateTimeIn) }}</h3>
+  <h3>Clocked In ({{ checkDate(timeCard.dateTimeIn) }})</h3>
+  <h3>{{ formatTime(timeCard.dateTimeOut) }}</h3>
+  <h3>Clocked Out ({{ checkDate(timeCard.dateTimeOut) }})</h3>
 </template>
 
 <script>
 export default {
-    created(){
-           console.log(this.timeCard.dateTimeIn);
-    },
-props: {
+  created() {
+    console.log(this.timeCard.dateTimeIn);
+  },
+  props: {
     timeCard: {
-        type: Object,
-        required: true
-    }
-},
-methods:{
-      formatTime(dateTime){
-        const date = new Date(dateTime);
-        const formattedTime = date.toLocaleTimeString([], {
-          hour: '2-digit',
-          minute: '2-digit',
-          hour12: true
-        });
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    formatTime(dateTime) {
+      const date = new Date(dateTime);
+      const formattedTime = date.toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: true,
+      });
 
-        return `${formattedTime}`;
-      },
-      formatDate(dateTime){
-        const date = new Date(dateTime);
+      return `${formattedTime}`;
+    },
+    formatDate(dateTime) {
+      const date = new Date(dateTime);
 
-        const formattedDate = date.toLocaleDateString('en-us', {
-          month: '2-digit',
-          day: '2-digit',
-          year: 'numeric'
-        });
+      const formattedDate = date.toLocaleDateString("en-us", {
+        month: "2-digit",
+        day: "2-digit",
+        year: "numeric",
+      });
 
-      
-        
-        return `${formattedDate}`;
-      },
+      return `${formattedDate}`;
+    },
 
-      checkDate(date){
-        const today = new Date.now;
-       if( this.formatDate(today) == this.formatDate(date)){
+    checkDate(date) {
+      const today = new Date();
+      const yesterday = new Date();
+      yesterday.setDate(today.getDate() - 1);
+      const formattedDate = this.formatDate(date);
+      const formattedToday = this.formatDate(today);
+      const formattedYesterday = this.formatDate(yesterday);
+      if (formattedDate === formattedToday) {
         return "Today";
-       }
-
-
+      } else if (formattedDate === formattedYesterday) {
+        return "Yesterday";
+      } else {
+        return "Other";
       }
-    }
-}
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
