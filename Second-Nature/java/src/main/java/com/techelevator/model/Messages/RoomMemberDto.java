c3109ea990ca614798f47ee74967d33b4560d28c
nathandickinson32 <nathandickinson32@gmail.com>
2025-03-23 19:50:14 -0400

    added room member dto

---
+++ Second-Nature/java/src/main/java/com/techelevator/model/Messages/RoomMemberDto.java
@@ -0,0 +1,22 @@
+package com.techelevator.model.Messages;
+
+public class RoomMemberDto {
+    private int roomId;
+    private int userId;
+
+    public int getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(int roomId) {
+        this.roomId = roomId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+}
